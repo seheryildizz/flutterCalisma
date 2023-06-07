cfa7affed9ef14482f18fbb23754ec40f00326c2
Seher YILDIZ <89248368+seheryildizz@users.noreply.github.com>
2023-06-07 17:46:41 +0300

    Delete bolum1 directory

--- bolum1/num_boolean.dart
+++
@@ -1,8 +0,0 @@
-void main(List<String> args) {
-  int kilo = 56.5.toInt();
-  print(kilo);
-
-  // int numara = null;    değer null olamaz
-  int? numara = null; //ancak soru işareti kullanırsak olabilir.
-  print(numara);
-}
